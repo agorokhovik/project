319195c274ada6001ef28f80b7043cac5b24fd70
Angie <angelina.gorokhovik@gmail.com>
2020-07-05 15:49:20 +0300

    added conditions and loops

--- Movie app/js/script.js
+++ Movie app/js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Приложение по учету просмотренных фильмов
+// Приложение по учету просмотренных фильмов (консольное приложение)
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -12,12 +12,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b; 
+        console.log('done!');
+    } else {
+        console.log('error');
+        i--; //если условие не выполнилось вернемся на одну итерацию цикла назад и зададим вопросы
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
